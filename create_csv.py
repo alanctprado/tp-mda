fd65c9d1681ef4758e856079d7c529ff1d694149
marcotuliopin <marcotuliopin@gmail.com>
2024-06-16 14:17:16 -0300

    Remove unecessary comments

--- create_csv.py
+++ create_csv.py
@@ -16,9 +16,7 @@ for filename in os.listdir(directory):
     if filename.endswith(".json"):
         with open(os.path.join(directory, filename)) as f:
             data = json.load(f)
-            # convert normal dict to defaultdict
             data = defaultdict(lambda: 'missing', data)
-            # reorder data by fields and write to csv
             with open('output.csv', 'a', newline='') as output_file:
                 writer = csv.writer(output_file)
                 writer.writerow([data[field] for field in header])
