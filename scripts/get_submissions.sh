d664130d12ba60a5d1e63c818d66d1a9141622a1
Alan Prado <alanctprado@gmail.com>
2024-07-06 20:45:35 -0300

    Minor change to submissions script

--- scripts/get_submissions.sh
+++ scripts/get_submissions.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/sh
 
-export MDABASEDIR=/home/alan/tp-mda/
-
-if [ -d "venv" ]; then source ../venv/bin/activate; fi
+export MDABASEDIR=/home/alanp/tp-mda/
 
 if [ ! -d "../data/submissions" ]; then mkdir ../data/submissions; fi
 
-num_files="$(ls $MDABASEDIR/data/active_users | wc -l)"
-echo $num_files
+num_files="$(ls ${MDABASEDIR}data/active_users | wc -l)"
 
 for user in ../data/active_users/*
 do
     handle=$(echo $user | sed 's:..\/data\/active_users\/\(.*\)\.json:\1:')
+    echo $handle
     if [ ! -f ../data/submissions/$handle.json ]
     then
-        python3 get_submissions.py $handle ../data/submissions
+        python3 get_submissions.py $handle ../data/submissions 2>&1 /dev/null
         sleep 2
     fi
-    echo "DONE"
 done | tqdm --total=$num_files >> /dev/null
-
-if [ -d "venv" ]; then deactivate; fi
