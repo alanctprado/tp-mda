c8f1fc7cb65ebf355d9bc840dbc7967f1ba46aeb
Denilson <denilsonmvs@gmail.com>
2024-07-11 16:30:58 -0300

    Adicionando handle de quem submeteu

--- create_submissions_csv.py
+++ create_submissions_csv.py
@@ -10,8 +10,10 @@ with open('submissions.csv', 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=header)
     writer.writeheader()
     for json_file in os.listdir(directory):
+        handle = json_file[:-5]
         with open(os.path.join(directory, json_file), 'r') as f:
             data = json.load(f)
             for item in data:
                 row_data = item.copy()
+                row_data["handle"] = handle
                 writer.writerow(row_data)
